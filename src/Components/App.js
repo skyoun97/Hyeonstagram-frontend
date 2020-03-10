83af375bf919f5f18227b6673222ea4c1f8c3864
전현성 <skyoun97@gmail.com>
2020-03-10 14:25:59 +0100

    Frontend - Setting Apollo Client

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import GlobalStyles from "../Styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import AppRouter from "./Router";
 
@@ -8,7 +8,7 @@ export default () => (
   <ThemeProvider theme={Theme}>
     <>
       <GlobalStyles />
-      <AppRouter isLoggedIn={!false} />
+      <AppRouter isLoggedIn={false} />
     </>
   </ThemeProvider>
 );
